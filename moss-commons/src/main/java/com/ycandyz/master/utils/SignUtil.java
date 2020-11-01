f2ee0b7c8e183bf9c79e9cd3465bfa1b2aa714fc
wang yang <sunone.wang@gmial.com>
2020-11-01 18:11:52 +0800

    增加参数验签功能

---
+++ moss-commons/src/main/java/com/ycandyz/master/utils/SignUtil.java
@@ -0,0 +1,53 @@
+package com.ycandyz.master.utils;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.SortedMap;
+
+/**
+ * @author: WangYang
+ * @Date: 2020/11/1 10:57
+ * @Description:
+ */
+
+
+@Slf4j
+public class SignUtil {
+
+    /**
+     * @param params 所有的请求参数都会在这里进行排序加密
+     * @return 验证签名结果
+     */
+    public static boolean verifySign(SortedMap<String, Object> params,String key) {
+
+        Object urlSign = params.get("sign");
+        log.info("Url Sign : {}", urlSign);
+        if (params == null || StrUtil.isEmpty(urlSign.toString())) {
+            return false;
+        }
+        //把参数加密
+        String paramsSign = getParamsSign(params,key);
+        log.info("Param Sign : {}", paramsSign);
+        return !StrUtil.isEmpty(paramsSign) && urlSign.equals(paramsSign);
+    }
+
+    /**
+     * @param params 所有的请求参数都会在这里进行排序加密
+     * @return 得到签名
+     */
+    public static String getParamsSign(SortedMap<String, Object> params,String key) {
+        //要先去掉 Url 里的 Sign
+        params.remove("sign");
+        String timestamp = params.get("timestamp").toString();
+        String paramsJsonStr = JSONObject.toJSONString(params);
+        String skey = timestamp + key;
+        String md5Key = "key=" + SecureUtil.md5(skey);
+        String paramsSign = paramsJsonStr + md5Key;
+
+        return SecureUtil.md5(paramsSign);
+    }
+
+}
---
+++ moss-controller/src/main/java/com/ycandyz/master/config/SignAuthFilter.java
@@ -0,0 +1,78 @@
+package com.ycandyz.master.config;
+
+import com.alibaba.fastjson.JSONObject;
+
+import com.ycandyz.master.domain.BodyReaderHttpServletRequestWrapper;
+import com.ycandyz.master.utils.SignUtil;
+import com.ycandyz.master.utils.HttpUtils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.SortedMap;
+
+/**
+ * @author: WangYang
+ * @Date: 2020/11/1 10:47
+ * @Description:
+ */
+
+@Slf4j
+@Component
+public class SignAuthFilter implements Filter {
+
+    @Value(value = "${sign.authConfigSecret}")
+    private String authConfigSecret;
+
+    static final String FAVICON = "/favicon.ico";
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        log.info("初始化 SignAuthFilter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        // 防止流读取一次后就没有了, 所以需要将流继续写出去
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletRequest requestWrapper = new BodyReaderHttpServletRequestWrapper(request);
+        //获取图标不需要验证签名
+        if (FAVICON.equals(requestWrapper.getRequestURI())) {
+            filterChain.doFilter(request, response);
+        } else {
+            //获取全部参数(包括URL和body上的)
+            SortedMap<String, Object> allParams = HttpUtils.getAllParams(requestWrapper);
+            //对参数进行签名验证
+            boolean isSigned = SignUtil.verifySign(allParams,authConfigSecret);
+
+            if (isSigned) {
+                log.info("签名通过");
+                filterChain.doFilter(requestWrapper, response);
+            } else {
+                log.info("参数校验出错");
+                //校验失败返回前端
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("application/json; charset=utf-8");
+                PrintWriter out = response.getWriter();
+                JSONObject resParam = new JSONObject();
+                resParam.put("msg", "参数校验出错");
+                resParam.put("success", "false");
+                out.append(resParam.toJSONString());
+            }
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        log.info("销毁 SignAuthFilter");
+    }
+
+    }
