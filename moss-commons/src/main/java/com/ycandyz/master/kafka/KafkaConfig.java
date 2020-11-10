8e74feef7c40d76310f4c2c21ee203d65c50d668
gongxipeng <gongxipeng@ycandyz.com>
2020-11-10 18:28:10 +0800

    介入kafka

---
+++ moss-commons/src/main/java/com/ycandyz/master/kafka/KafkaConfig.java
@@ -0,0 +1,40 @@
+package com.ycandyz.master.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServers;
+
+    @Bean //创建一个kafka管理类，相当于rabbitMQ的管理类rabbitAdmin,没有此bean无法自定义的使用adminClient创建topic
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> props = new HashMap<>();
+        //配置Kafka实例的连接地址
+        //kafka的地址，不是zookeeper
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
+        KafkaAdmin admin = new KafkaAdmin(props);
+        return admin;
+    }
+
+    @Bean  //kafka客户端，在spring中创建这个bean之后可以注入并且创建topic,用于集群环境，创建对个副本
+    public AdminClient adminClient() {
+        return AdminClient.create(kafkaAdmin().getConfig());
+    }
+
+
+}
