ca0a8e30f70e0036bbdcb165259f3585e5d88349
sangang <hesangang@ycandyz.com>
2020-11-18 19:25:58 +0800

    添加优惠卷接口

---
+++ moss-commons/src/main/java/com/ycandyz/master/domain/model/coupon/CouponActivityModel.java
@@ -0,0 +1,77 @@
+package com.ycandyz.master.domain.model.coupon;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ycandyz.master.entities.coupon.CouponActivity;
+import com.ycandyz.master.validation.ValidatorContract;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+/**
+ * <p>
+ * @Description 发卷宝 Resp
+ * </p>
+ *
+ * @author SanGang
+ * @since 2020-11-17
+ * @version 2.0
+ */
+@Getter
+@Setter
+@ApiModel(description="发卷宝-参数")
+public class CouponActivityModel {
+
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "活动编号")
+    @NotNull(message = "活动编号不能为空",groups = {ValidatorContract.OnUpdate.class})
+    private String activityNo;
+
+    @Size(max = 20, message = "活动名称不能大于20个字。",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @NotNull(message = "活动名称不能为空",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "活动名称")
+    private String name;
+
+    @Size(max = 4, message = "活动入口名称不能大于4个字。",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @NotNull(message = "活动入口名称不能为空",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "活动入口名称")
+    private String joinName;
+
+    @NotNull(message = "活动开始时间不能为空",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "活动开始时间")
+    private Long beginAt;
+
+    @NotNull(message = "活动结束时间不能为空",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "活动结束时间")
+    private Long endAt;
+
+    @Range(min = 1, max = 1000000, message = "赠送上限人数范围为1-1000000之间", groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @NotNull(message = "活赠送上限人数不能为空",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "赠送上限人数")
+    private Integer heightLimit;
+
+    @Range(min = 0, max = 2, message = "活动参与人类型必须为0-2之间", groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @NotNull(message = "活动参与人不能为空",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "活动参与人：0:全部用户，1:仅老用户，2:仅新用户")
+    private Integer joinType;
+
+    @Range(min = 0, max = 2, message = "活页面设置必须为0-2之间", groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "页面设置：0店铺页面")
+    private Integer pageType;
+
+    @NotNull(message = "优惠卷编号不能为空",groups = {ValidatorContract.OnUpdate.class, ValidatorContract.OnCreate.class})
+    @ApiModelProperty(value = "优惠卷编号")
+    private List<String> ticketList;
+
+
+}
