3da76a724f787bd5e0bfc83dd2fc81312d535017
sangang <hesangang@ycandyz.com>
2020-11-27 16:51:18 +0800

    去掉下划线转驼峰

--- moss-controller/src/main/java/com/ycandyz/master/config/WebMvcConfiguration.java
+++ moss-controller/src/main/java/com/ycandyz/master/config/WebMvcConfiguration.java
@@ -1,6 +1,5 @@
 package com.ycandyz.master.config;
 
-import com.ycandyz.master.underline.CustomHandlerMethodArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -62,7 +61,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(CurrentUserHandlerMethodArgReslover);
-        resolvers.add(new CustomHandlerMethodArgumentResolver());
     }
 
 //    @Override
