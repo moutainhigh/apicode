aef4b11c669b5e0324960cdbc7713e26e41e1642
lizhuo <lizhuo@ycandyz.com>
2020-12-01 11:01:25 +0800

    发布记录查询加分页

--- moss-controller/src/main/java/com/ycandyz/master/controller/miniprogram/OrganizeMpReleaseController.java
+++ moss-controller/src/main/java/com/ycandyz/master/controller/miniprogram/OrganizeMpReleaseController.java
@@ -1,16 +1,20 @@
 package com.ycandyz.master.controller.miniprogram;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ycandyz.master.api.BasePageResult;
 import com.ycandyz.master.api.BaseResult;
 import com.ycandyz.master.model.miniprogram.OrganizeMpReleaseVO;
 import com.ycandyz.master.service.miniprogram.IOrganizeMpReleaseService;
 import com.ycandyz.master.service.miniprogram.MpChooseStyleService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.ycandyz.master.api.CommonResult;
 import com.ycandyz.master.entities.miniprogram.OrganizeMpRelease;
@@ -50,10 +54,15 @@ public class OrganizeMpReleaseController extends BaseController<OrganizeMpReleas
 //    }
 
     @ApiOperation(value = "查询企业小程序全部发布记录", tags = "企业小程序DIY配置")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="page_size",value="每页显示条数",dataType="Long"),
+            @ApiImplicitParam(name="page",value="当前页",dataType="Long")
+    })
     @GetMapping
-    public CommonResult<BaseResult<List<OrganizeMpReleaseVO>>> list() {
-        List<OrganizeMpReleaseVO> list = organizeMpReleaseService.listAll();
-        return CommonResult.success(new BaseResult<>(list));
+    public CommonResult<BasePageResult<Page<OrganizeMpReleaseVO>>> list(@RequestParam(value = "page_size",defaultValue = "10",required = false) Long page_size,
+                                                                        @RequestParam(value = "page",defaultValue = "1",required = false) Long page) {
+        Page<OrganizeMpReleaseVO> list = organizeMpReleaseService.listAll(page_size,page);
+        return CommonResult.success(new BasePageResult<>(list));
     }
 
 }
