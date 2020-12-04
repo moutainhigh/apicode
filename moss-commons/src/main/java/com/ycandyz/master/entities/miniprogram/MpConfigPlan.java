56df129b0b289f4947c5db18cf2aba6de6ae29d2
wang yang <sunone.wang@gmial.com>
2020-12-04 11:31:02 +0800

    修复更新时间

--- moss-commons/src/main/java/com/ycandyz/master/entities/miniprogram/MpConfigPlan.java
+++ moss-commons/src/main/java/com/ycandyz/master/entities/miniprogram/MpConfigPlan.java
@@ -48,12 +48,10 @@ public class MpConfigPlan extends Model {
 
    @ApiModelProperty(value = "创建时间")
    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-   @TableField(value = "create_time", fill = FieldFill.INSERT)
    private Date createTime;
 
    @ApiModelProperty(value = "修改时间")
    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-   @TableField(value = "update_time", fill = FieldFill.INSERT)
    private Date updateTime;
 
 
