f99b0b51faea3ad887b84cb85c75f1e8062435ca
wangwenxin <1@qq.com>
2021-01-12 21:50:35 +0800

    生成id

--- moss-commons/src/main/java/com/ycandyz/master/utils/IDGeneratorUtils.java
+++ moss-commons/src/main/java/com/ycandyz/master/utils/IDGeneratorUtils.java
@@ -3,6 +3,8 @@ package com.ycandyz.master.utils;
 import com.ycandyz.master.utils.id.SnowflakeIdHelper;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Random;
+
 /**
  * <P>
  * 用途:生成数字id
@@ -17,10 +19,23 @@ public class IDGeneratorUtils {
 
     @Value("${id.datacenterId}")
     private static long datacenterId;
-
+    public static final String CAR_CODE_PREFIX = "P";
     public static long getLongId(){
         final SnowflakeIdHelper snowflakeIdHelper = SnowflakeIdHelper.getInstance(workerId,datacenterId);
         long id = snowflakeIdHelper.nextId();
         return id;
+    }
+
+    public static String getStrId() {
+        Random random = new Random(System.currentTimeMillis());
+        for (int j=0;j<20;j++){
+            String uuid = "";
+            for (int i=0;i<15;i++){
+                int subNum = random.nextInt(9);
+                uuid+=subNum;
+            }
+            return CAR_CODE_PREFIX+uuid;
+        }
+        return null;
     }
 }
